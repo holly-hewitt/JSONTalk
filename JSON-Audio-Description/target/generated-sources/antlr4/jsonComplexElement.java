5aefc24610571ca2723f6a963744776a0570d854
holly-hewitt <holly-hewitt@hotmail.co.uk>
2022-12-05 13:08:55 +0000

    Added methods for type description

--- JSON-Audio-Description/target/generated-sources/antlr4/jsonComplexElement.java
+++ JSON-Audio-Description/target/generated-sources/antlr4/jsonComplexElement.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 public class jsonComplexElement extends jsonElement {
-	
+
 	private int fieldNo;
 	private ArrayList<jsonObject> childObjs;
-	private ArrayList<jsonArray> childArrs;	
+	private ArrayList<jsonArray> childArrs;
+	private LinkedHashMap<String, ArrayList <jsonElement>> children;
 
 	/**
 	 * @param name
@@ -17,7 +20,7 @@ public class jsonComplexElement extends jsonElement {
 		this.childObjs = new ArrayList<jsonObject>();
 		this.childArrs = new ArrayList<jsonArray>();
 	}
-	
+
 	public jsonComplexElement(jsonParser.ValueContext ctx, int fieldNo) {
 		super(ctx);
 		this.fieldNo = fieldNo;
@@ -38,51 +41,71 @@ public class jsonComplexElement extends jsonElement {
 	public ArrayList<jsonArray> getChildArrs() {
 		return childArrs;
 	}
-	
-	
+
 	public void addChildObj(jsonObject childObj) {
 		childObjs.add(childObj);
 	}
-	
-	public void addChildArr (jsonArray childArr) {
+
+	public void addChildArr(jsonArray childArr) {
 		childArrs.add(childArr);
 	}
-	
-	//description methods
-	
+
+	// description methods
+
 	public String elemDescription() {
 		if (name.equals("")) {
 			return "";
 		}
 		String description = String.format(("\"%s is an %s which contains %d field\", name, typeName, fieldNo"));
-		if(childObjs.size()>0) {
+		if (childObjs.size() > 0) {
 			description += listChildObjects();
 		}
-		if(childArrs.size()>0) {
+		if (childArrs.size() > 0) {
 			description += listChildArrs();
 		}
 		return description;
 	}
 
 	private String listChildArrs() {
-		
-		if(childArrs.size() == 1) {
-			String description = "1 field is an array";
-			if (childArrs.get(0).getName() != "") {
-				description += "named, "
-			}
-		}
-		String description = String.format("%d field%s", childArrs.size(), (childArrs.size()==1 ? " is an array, ": "s are arrays, "));
-		
-		
-		// TODO Auto-generated method stub
-		return null;
+		if(childArrs.size()>0) {
+			return String.format("%d field%s", childArrs.size(),
+					(childArrs.size() == 1 ? " is an array, " : "s are arrays, "));
+		}else {
+			return "";
+		}		
 	}
 
 	private String listChildObjects() {
 		// TODO Auto-generated method stub
+		if(childObjs.size()>0) {
+			return String.format("%d field%s", childArrs.size(),
+					(childObjs.size() == 1 ? " is an object, " : "s are objects, "));
+		}
 		return null;
 	}
 	
-
+	private String listAllChildren() {
+		
+		String description = "";
+		
+		Set<String> types = children.keySet();
+		for (String type : types) {
+			int numOfType = children.get(type).size();
+			if (numOfType == 1) {
+				description += String.format("1 field is a %s value. ", type);
+			}else {
+				description += String.format("%d fields are %s values. ", numOfType, type);
+			}
+		}
+		
+		return description;
+	}
+	
+	public String elementDescription(boolean describeTypes) {
+		String description = elemDescription();
+		if (describeTypes) {
+			description += listAllChildren();
+		}
+		return description;
+	}
 }
