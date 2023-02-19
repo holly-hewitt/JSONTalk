8ee1e6cd1b2466ffb4866d380dc567ceca4ed659
holly-hewitt <holly-hewitt@hotmail.co.uk>
2023-02-19 23:34:25 +0000

    Updated documentation.
    Still need to add some more comments

--- JSON-Audio-Description/doc/type-search-index.js
+++ JSON-Audio-Description/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"descriptionLevel"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"jsonComplexElement"}];updateSearchResults();
